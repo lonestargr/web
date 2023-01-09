3a8752d9608891111353fc720f6c7b1a47b781ee
lonestargr <lonestarcarpetinstall@gmail.com>
2023-01-08 20:31:50 -0600

    Update favicon and title

--- src/components/About.js
+++ src/components/About.js
@@ -45,7 +45,7 @@ function About() {
             <Image
               src={ logo }
             />
-            <Title style={{padding: '40px 20px', margin: '0', textAlign: 'center'}} level={5}>We are Lone Star General Remodeling. The family business has been serving the Houston area for well over a decade now, using our services to give customers great quality at an affordable rate. We offer general remodeling services that include <u>LVT installation, Kitchen & Bath remodels, inventor flips and much more!</u> Call us today and set up a <em>free estimate!</em></Title>
+            <Title style={{padding: '40px 20px', margin: '0', textAlign: 'center'}} level={5}>We are Lone Star General Remodeling. The family business has been serving the Houston area for well over a decade now, using our services to give customers great quality at an affordable rate. We offer general remodeling services that include <u>LVT installation, Kitchen & Bath remodels, invenstor flips and much more!</u> Call us today and set up a <em>free estimate!</em></Title>
       </div>
     </div>
   );
